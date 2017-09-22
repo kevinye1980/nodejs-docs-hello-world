ec806b7dc884c5766145f4064b5f6850232a6fc2
Kevin Ye <chunye@live.com>
2017-09-22 12:37:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("NodeJS Hello World from 2nd commit to Jenkins + Docker!");
+    response.end("NodeJS Hello World from 3rd commit to Jenkins + Docker!");
 
 });
 
